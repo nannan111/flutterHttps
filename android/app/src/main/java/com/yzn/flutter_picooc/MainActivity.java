b8c7f992fea6727345e6ebaf23058aebed1c4022
yangzhinan <yangzhinan@picooc.com>
2019-08-12 14:33:53 +0800

    提交代码

---
+++ android/app/src/main/java/com/yzn/flutter_picooc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yzn.flutter_picooc;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
